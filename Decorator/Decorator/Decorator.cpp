5e498b64a4834946f615694067415c1a0557c6f8
garbich <131876643+garbich@users.noreply.github.com>
2023-11-08 20:30:08 +0200

    Add files via upload

---
+++ Decorator/Decorator/Decorator.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+class TextEditor {
+
+	string text;
+
+public:
+	TextEditor() {
+	}
+
+
+	TextEditor(TextEditor* textEditor) {
+		this->text = textEditor->getText();
+	}
+
+	string getText() {
+		return text;
+	}
+
+	void setText(string text) {
+		this->text = text;
+	}
+};
+
+class SimpleTextEditor : public TextEditor {
+public:
+	SimpleTextEditor() : TextEditor() {
+		setText("base text");
+	}
+
+	SimpleTextEditor(TextEditor* textEditor) : TextEditor(textEditor) {
+		setText(textEditor->getText() + " base text");
+	}
+
+
+};
+
+class SpellChecker : public TextEditor {
+public:
+	SpellChecker() : TextEditor() {}
+	SpellChecker(TextEditor* textEditor) : TextEditor(textEditor) {
+		setText(textEditor->getText() + " spell text");
+	}
+};
+
+class AutoSave : public TextEditor {
+public:
+	AutoSave() : TextEditor() {}
+	AutoSave(TextEditor* textEditor) : TextEditor(textEditor) {
+		setText(textEditor->getText() + " auto save text");
+	}
+};
+
+int main() {
+
+	TextEditor* editor = new SimpleTextEditor();
+
+
+	editor = new SpellChecker(editor);
+	editor = new AutoSave(editor);
+	editor = new SpellChecker(editor);
+	editor = new SimpleTextEditor(editor);
+
+	cout << editor->getText();
+}
---
+++ Facade/Facade/Facade.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+class TV {
+public:
+
+	void onTV() {
+		cout << "TV was on" << endl;
+	}
+
+	void offTV() {
+		cout << "TV was off" << endl;
+	}
+};
+
+
+class Conditioning {
+public:
+	void onConditioning() {
+		cout << "Conditioning was on. temperature = 20 degree" << endl;
+	}
+
+	void offConditioning() {
+		cout << "conditioning was off" << endl;
+	}
+};
+
+class Light {
+public:
+
+	void onLight() {
+		cout << "Light was on" << endl;
+	}
+
+	void offLight() {
+		cout << "Light was off" << endl;
+	}
+};
+
+class SmartHomeFacade {
+	TV* tv;
+	Conditioning* conditioning;
+	Light* light;
+public:
+
+	void watchTV() {
+		tv->onTV();
+		conditioning->onConditioning();
+		light->onLight();
+	}
+
+	void turnOffEverything() {
+		tv->offTV();
+		conditioning->offConditioning();
+		light->offLight();
+	}
+};
+
+int main() {
+	SmartHomeFacade smartHome;
+
+	smartHome.watchTV();
+
+	cout << endl;
+
+	smartHome.turnOffEverything();
+}
